8d41221eebb52344547cd8ed4a96786b6905fe33
Alexander Krenz <krenzalexander@proton.me>
2025-08-03 09:52:57 +0200

    feat: solve 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+    let arr = array.slice();
+    for (const item of items) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
