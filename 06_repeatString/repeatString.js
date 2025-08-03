80e0f4f64d30069a8a79f4c3295121a87400b166
Alexander Krenz <krenzalexander@proton.me>
2025-08-03 09:32:12 +0200

    feat: solve 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (str, n) {
+    if (n < 0) return "ERROR";
+    let repeated = "";
+    while (n > 0) {
+        repeated += str;
+        n--;
+    }
 
+    return repeated;
 };
 
 // Do not edit below this line
