80d9d6e12240f969a0f0fc9f1804d368e83e95df
Alexander Krenz <krenzalexander@proton.me>
2025-08-05 17:20:52 +0200

    feat: solve 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = +n;
+    if (n < 0 || isNaN(n)) return "OOPS";
 
+    switch (n) {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        default:
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
