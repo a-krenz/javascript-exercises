631846b16dad9fae7e3e085780440b6f45d200a0
Alexander Krenz <krenzalexander@proton.me>
2025-08-05 17:03:30 +0200

    feat: solve 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let charArray = str.toLowerCase().split("");
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    charArray = charArray.filter((item) => alphanumerical.includes(item));
+
+    const middleIndex = (charArray.length - 1) / 2;
+    const firstHalf = charArray.slice(0, Math.floor(middleIndex) + 1).join("");
+    const secondHalf = charArray.slice(Math.ceil(middleIndex)).reverse().join("");
+    return firstHalf === secondHalf;
 };
 
 // Do not edit below this line
